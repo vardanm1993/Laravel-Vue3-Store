68021aedb3bc9dd8ba4744ef637fd6350d6dc391
vardanm1993 <vardanm1993@gmail.com>
2023-02-07 18:54:01 +0400

    Added Category CRUD

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -152,7 +152,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('category.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-list-ul"></i>
                             <p>Categories</p>
                         </a>
